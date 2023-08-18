7de6ca34c39aa89d7f5e4634c8e0c9f64828a550
2017tmxkxm <2017tmxkxm@gmail.com>
2023-08-18 17:44:55 +0900

    testController

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.TestRequestBodyDTO;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+	
+	@GetMapping
+	public String testController() {
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/testGetMapping")
+	public String testControllerWithPath() {
+		return "Hello World! testGetMapping!";
+	}
+	
+	// @PathVariable
+	@GetMapping("/{id}")
+	public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+		return "Hello World! + ID " + id;
+	}
+	
+	// @RequestParam
+	@GetMapping("/testRequestParam")
+	public String testControllerRequestParam(@RequestParam(required = false) int id) {
+		return "Hello World! + ID " + id;
+	}
+	
+	// @RequestBody
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+	}
+}
