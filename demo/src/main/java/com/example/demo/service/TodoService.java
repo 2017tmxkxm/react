a04ff14140b76bb748ee287f03ec94c6fa537bce
2017tmxkxm <2017tmxkxm@gmail.com>
2023-08-20 14:53:02 +0900

    TodoService > create 생성

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,11 +1,48 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class TodoService {
 
+	@Autowired
+	private TodoRepository repository;
+	
 	public String testService() {
-		return "Test Servcie";
+		// TodoEntity 생성
+		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+		// TodoEntity 저장
+		repository.save(entity);
+		// TodoEntity 검색
+		TodoEntity saveEntity = repository.findById(entity.getId()).get();
+		return saveEntity.getTitle();
+	}
+	
+	public List<TodoEntity> create(final TodoEntity entity) {
+		// Validation
+		if(entity == null) {
+			log.warn("Entity cannot be null.");
+			throw new RuntimeException("Entity cannot be null.");
+		}
+		
+		if(entity.getUserId() == null) {
+			log.warn("Unknown user.");
+			throw new RuntimeException("Unknown user.");
+		}
+		
+		repository.save(entity);
+		
+		log.info("Entity Id : {} is saved.", entity.getId());
+		
+		return repository.findByUserId(entity.getUserId());
 	}
 }
