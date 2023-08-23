7974e805c14a75638b411d08944e8e109b747bc7
2017tmxkxm <2017tmxkxm@gmail.com>
2023-08-23 17:53:52 +0900

    frontend > App.js addItem, deleteItem 추가

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,27 +1,32 @@
+import { Container, List, Paper } from "@mui/material";
+import React, { useState } from "react";
+import AddTodo from './AddTodo';
 import './App.css';
 import Todo from './Todo';
-import React, { useState } from "react";
-import { List, Paper } from "@mui/material";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: "0",
-      title: "Hello World 1",
-      doen: true
-    },
-    {
-      id: "1",
-      title: "Hello World 2",
-      doen: false
-    },
-  ]);
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    item.id = "Id-" + items.length; // key를 위한 id
+    item.done = false; // done 초기화
+    // 업데이트는 반드시 setItems로 하고 새 배열을 만들어야 한다.
+    setItems([...items, item]);
+    console.log("items: ", items);
+  }
+
+  const deleteItem = (item) => {
+    // 삭제할 아이템을 찾는다.
+    const newItems = items.filter(e => e.id !== item.id);
+    // 삭제할 아이템을 제외한 아이템을 다시 배열에 저장한다.
+    setItems([...newItems]);
+  }
 
   let todoItems = items.length > 0 && (
     <Paper style={{ margin: 16 }}>
       <List>
         {items.map((item) => (
-          <Todo item={item} key={item.id} />
+          <Todo item={item} key={item.id} deleteItem={deleteItem} />
         ))}  
       </List>
     </Paper>
@@ -29,7 +34,10 @@ function App() {
 
   return (
     <div className="App">
-      {todoItems}
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
     </div>
   );
 }
