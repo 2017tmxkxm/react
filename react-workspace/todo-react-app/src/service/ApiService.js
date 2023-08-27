33bd36e619b6335a7978dc66810d4cb1c70dfcf7
2017tmxkxm <2017tmxkxm@gmail.com>
2023-08-27 14:28:35 +0900

    fronted > ApiService.js : 액세스 토큰 저장

--- react-workspace/todo-react-app/src/service/ApiService.js
+++ react-workspace/todo-react-app/src/service/ApiService.js
@@ -1,14 +1,20 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
+    let headers = new Headers({"Content-Type": "application/json",});
+
+    // 로컬 스토리지에서 ACCESS TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
-    
+
     if(request) {
         // GET method
         options.body = JSON.stringify(request);
@@ -33,6 +39,7 @@ export function singin(userDTO) {
     return call("/auth/signin", "POST", userDTO)
         .then((response) => {
             if(response.token) {
+                localStorage.setItem("ACCESS_TOKEN", response.token);
                 // token이 존재하는 경우 Todo 화면으로 리다이렉트
                 window.location.href = "/";
             }
